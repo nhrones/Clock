7a8f697ece0fe5ff57aecdee7b6309e2dd56be0f
NDH <ndhrones@gmail.com>
2024-04-09 21:32:44 -0400

    r

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-// deno-lint-ignore-file
+//@ts-nocheck
 
 // src/constants.ts
 var NUMBER_SPACING = 16;
@@ -7,7 +7,6 @@ var DOT_HEIGHT = 16;
 var MATRIX_WIDTH = 4;
 var MATRIX_HEIGHT = 7;
 var PIXELS = [
-  // 'zero'
   [
     [1, 1, 1, 1],
     [1, 0, 0, 1],
@@ -17,7 +16,6 @@ var PIXELS = [
     [1, 0, 0, 1],
     [1, 1, 1, 1]
   ],
-  // 'one'
   [
     [0, 0, 0, 1],
     [0, 0, 0, 1],
@@ -27,7 +25,6 @@ var PIXELS = [
     [0, 0, 0, 1],
     [0, 0, 0, 1]
   ],
-  // 'two'
   [
     [1, 1, 1, 1],
     [0, 0, 0, 1],
@@ -37,7 +34,6 @@ var PIXELS = [
     [1, 0, 0, 0],
     [1, 1, 1, 1]
   ],
-  // 'three'
   [
     [1, 1, 1, 1],
     [0, 0, 0, 1],
@@ -47,7 +43,6 @@ var PIXELS = [
     [0, 0, 0, 1],
     [1, 1, 1, 1]
   ],
-  // 'four'
   [
     [1, 0, 0, 1],
     [1, 0, 0, 1],
@@ -57,7 +52,6 @@ var PIXELS = [
     [0, 0, 0, 1],
     [0, 0, 0, 1]
   ],
-  // 'five'
   [
     [1, 1, 1, 1],
     [1, 0, 0, 0],
@@ -67,7 +61,6 @@ var PIXELS = [
     [0, 0, 0, 1],
     [1, 1, 1, 1]
   ],
-  // 'six'
   [
     [1, 1, 1, 1],
     [1, 0, 0, 0],
@@ -77,7 +70,6 @@ var PIXELS = [
     [1, 0, 0, 1],
     [1, 1, 1, 1]
   ],
-  // 'seven'
   [
     [1, 1, 1, 1],
     [0, 0, 0, 1],
@@ -87,7 +79,6 @@ var PIXELS = [
     [0, 0, 0, 1],
     [0, 0, 0, 1]
   ],
-  // 'eight'
   [
     [1, 1, 1, 1],
     [1, 0, 0, 1],
@@ -97,7 +88,6 @@ var PIXELS = [
     [1, 0, 0, 1],
     [1, 1, 1, 1]
   ],
-  // 'nine'
   [
     [1, 1, 1, 1],
     [1, 0, 0, 1],
@@ -110,34 +100,135 @@ var PIXELS = [
 ];
 
 // src/dotPool.ts
+function tickDots(thisTime) {
+  delta = (thisTime - lastTime) / 1000;
+  lastTime = thisTime;
+  updateDotPositions(delta);
+  testForCollisions(delta);
+}
+function activateDot(x, y) {
+  for (let idx = 0;idx < tailPointer + 2; idx++) {
+    if (posX[idx] === 0) {
+      posX[idx] = x;
+      posY[idx] = y;
+      lastX[idx] = x;
+      lastY[idx] = y;
+      velocityX[idx] = randomVelocity();
+      velocityY[idx] = randomVelocity();
+      if (idx > tailPointer)
+        tailPointer = idx;
+      break;
+    }
+  }
+}
+function renderDot(x, y, color) {
+  canvasCTX.fillStyle = color || Color;
+  canvasCTX.beginPath();
+  canvasCTX.arc(x, y, HalfRadius, 0, 2 * Math.PI, true);
+  canvasCTX.closePath();
+  canvasCTX.fill();
+}
+var updateDotPositions = function(delta) {
+  for (let i = 0;i < tailPointer + 2; i++) {
+    if (posX[i] === 0) {
+      continue;
+    }
+    velocityX[i] += CTX.GravityX * delta;
+    velocityY[i] += CTX.GravityY * delta;
+    posX[i] += velocityX[i] * delta;
+    posY[i] += velocityY[i] * delta;
+    if (posX[i] <= Radius || posX[i] >= width) {
+      if (posY[i] >= height - 2) {
+        posX[i] = 0;
+        if (i === tailPointer) {
+          tailPointer--;
+        }
+        continue;
+      } else {
+        if (posX[i] <= Radius) {
+          posX[i] = Radius;
+        }
+        if (posX[i] >= width) {
+          posX[i] = width;
+        }
+        velocityX[i] *= -CTX.Restitution;
+      }
+    }
+    if (posY[i] >= height) {
+      posY[i] = height;
+      velocityY[i] *= -CTX.Restitution;
+    }
+    if (posY[i] <= Radius) {
+      posY[i] = Radius;
+      velocityY[i] *= -CTX.Restitution;
+    }
+    renderFreeDot(i);
+  }
+};
+var testForCollisions = function(delta) {
+  for (let i = 0;i < tailPointer + 2; i++) {
+    if (posX[i] === 0) {
+      continue;
+    }
+    for (let j = 0;j < tailPointer + 2; j++) {
+      if (i === j) {
+        continue;
+      }
+      if (posX[j] === 0) {
+        continue;
+      }
+      distanceX = Math.abs(posX[i] - posX[j]);
+      distanceY = Math.abs(posY[i] - posY[j]);
+      thisDistanceSquared = distanceX ** 2 + distanceY ** 2;
+      if (thisDistanceSquared < Radius_Sqrd) {
+        if (newDistanceSquared(delta, i, j) > thisDistanceSquared) {
+          continue;
+        }
+        collideDots(i, j, distanceX, distanceY);
+      }
+    }
+  }
+};
+var collideDots = function(dotA, dotB, distanceX, distanceY) {
+  thisDistanceSquared = distanceX ** 2 + distanceY ** 2;
+  velocityDiffX = velocityX[dotA] - velocityX[dotB];
+  velocityDiffY = velocityY[dotA] - velocityY[dotB];
+  actualDistance = Math.sqrt(thisDistanceSquared);
+  ratioX = distanceX / actualDistance;
+  ratioY = distanceY / actualDistance;
+  impactSpeed = velocityDiffX * ratioX + velocityDiffY * ratioY;
+  velocityX[dotA] -= ratioX * impactSpeed;
+  velocityY[dotA] -= ratioY * impactSpeed;
+  velocityX[dotB] += ratioX * impactSpeed;
+  velocityY[dotB] += ratioY * impactSpeed;
+};
+var newDistanceSquared = function(delta, a, b) {
+  newDotAx = posX[a] + velocityX[a] * delta;
+  newDotAy = posY[a] + velocityY[a] * delta;
+  newDotBx = posX[b] + velocityX[b] * delta;
+  newDotBy = posY[b] + velocityY[b] * delta;
+  return Math.abs(newDotAx - newDotBx) ** 2 + Math.abs(newDotAy - newDotBy) ** 2;
+};
+var renderFreeDot = function(i) {
+  canvasCTX.beginPath();
+  canvasCTX.fillStyle = Color;
+  canvasCTX.strokeStyle = Color;
+  canvasCTX.lineWidth = Radius;
+  canvasCTX.moveTo(lastX[i] - Radius, lastY[i] - Radius);
+  canvasCTX.lineTo(posX[i] - Radius, posY[i] - Radius);
+  canvasCTX.stroke();
+  canvasCTX.closePath();
+  canvasCTX.fill();
+  lastX[i] = posX[i];
+  lastY[i] = posY[i];
+};
+var randomVelocity = function() {
+  return (Math.random() - 0.4) * CTX.MaxVelocity;
+};
 var CTX = {
-  /** 
-   * A false gravitational pull in the X direction    
-   * (positive = right and negative = left)   
-   * default = 0
-   */
   GravityX: 0,
-  /** 
-   * A gravitational pull in the positive Y direction(down to floor)     
-   * Have some fun! ... try a negative value
-   * default = 1600
-   */
   GravityY: 750,
-  /**
-   * The coefficient of restitution (COR) is the ratio     
-   * of the final to initial relative velocity between     
-   * two objects after they collide.    
-   * 
-   * This represents the amount of 'bounce' a dot will exibit.    
-   * 1.0 = full rebound, and 0.5 will rebound only    
-   * half as high as the distance fallen.    
-   * default = 0.8
-   */
   Restitution: 0.5,
-  /** 
-   * The Maximum Velocity that a dot may take when it recieves a random velocity.     
-   * default = 2400
-   */
   MaxVelocity: 750
 };
 var Radius = 14;
@@ -165,145 +256,18 @@ var lastX = Array(POOL_SIZE);
 var lastY = Array(POOL_SIZE);
 var velocityX = Array(POOL_SIZE);
 var velocityY = Array(POOL_SIZE);
-for (let i2 = 0; i2 < POOL_SIZE; i2++) {
-  posX[i2] = 0;
-  posY[i2] = 0;
-  lastX[i2] = 0;
-  lastY[i2] = 0;
-  velocityX[i2] = 0;
-  velocityY[i2] = 0;
+for (let i = 0;i < POOL_SIZE; i++) {
+  posX[i] = 0;
+  posY[i] = 0;
+  lastX[i] = 0;
+  lastY[i] = 0;
+  velocityX[i] = 0;
+  velocityY[i] = 0;
 }
 var tailPointer = 0;
 var lastTime = Date.now();
-function tickDots(thisTime) {
-  delta = (thisTime - lastTime) / 1e3;
-  lastTime = thisTime;
-  updateDotPositions(delta);
-  testForCollisions(delta);
-}
-function activateDot(x, y) {
-  for (let idx = 0; idx < tailPointer + 2; idx++) {
-    if (posX[idx] === 0) {
-      posX[idx] = x;
-      posY[idx] = y;
-      lastX[idx] = x;
-      lastY[idx] = y;
-      velocityX[idx] = randomVelocity();
-      velocityY[idx] = randomVelocity();
-      if (idx > tailPointer)
-        tailPointer = idx;
-      break;
-    }
-  }
-}
-function renderDot(x, y, color) {
-  canvasCTX.fillStyle = color || Color;
-  canvasCTX.beginPath();
-  canvasCTX.arc(x, y, HalfRadius, 0, 2 * Math.PI, true);
-  canvasCTX.closePath();
-  canvasCTX.fill();
-}
-function updateDotPositions(delta2) {
-  for (let i2 = 0; i2 < tailPointer + 2; i2++) {
-    if (posX[i2] === 0) {
-      continue;
-    }
-    velocityX[i2] += CTX.GravityX * delta2;
-    velocityY[i2] += CTX.GravityY * delta2;
-    posX[i2] += velocityX[i2] * delta2;
-    posY[i2] += velocityY[i2] * delta2;
-    if (posX[i2] <= Radius || posX[i2] >= width) {
-      if (posY[i2] >= height - 2) {
-        posX[i2] = 0;
-        if (i2 === tailPointer) {
-          tailPointer--;
-        }
-        continue;
-      } else {
-        if (posX[i2] <= Radius) {
-          posX[i2] = Radius;
-        }
-        if (posX[i2] >= width) {
-          posX[i2] = width;
-        }
-        velocityX[i2] *= -CTX.Restitution;
-      }
-    }
-    if (posY[i2] >= height) {
-      posY[i2] = height;
-      velocityY[i2] *= -CTX.Restitution;
-    }
-    if (posY[i2] <= Radius) {
-      posY[i2] = Radius;
-      velocityY[i2] *= -CTX.Restitution;
-    }
-    renderFreeDot(i2);
-  }
-}
-function testForCollisions(delta2) {
-  for (let i2 = 0; i2 < tailPointer + 2; i2++) {
-    if (posX[i2] === 0) {
-      continue;
-    }
-    for (let j = 0; j < tailPointer + 2; j++) {
-      if (i2 === j) {
-        continue;
-      }
-      if (posX[j] === 0) {
-        continue;
-      }
-      distanceX = Math.abs(posX[i2] - posX[j]);
-      distanceY = Math.abs(posY[i2] - posY[j]);
-      thisDistanceSquared = distanceX ** 2 + distanceY ** 2;
-      if (thisDistanceSquared < Radius_Sqrd) {
-        if (newDistanceSquared(delta2, i2, j) > thisDistanceSquared) {
-          continue;
-        }
-        collideDots(i2, j, distanceX, distanceY);
-      }
-    }
-  }
-}
-function collideDots(dotA, dotB, distanceX2, distanceY2) {
-  thisDistanceSquared = distanceX2 ** 2 + distanceY2 ** 2;
-  velocityDiffX = velocityX[dotA] - velocityX[dotB];
-  velocityDiffY = velocityY[dotA] - velocityY[dotB];
-  actualDistance = Math.sqrt(thisDistanceSquared);
-  ratioX = distanceX2 / actualDistance;
-  ratioY = distanceY2 / actualDistance;
-  impactSpeed = velocityDiffX * ratioX + velocityDiffY * ratioY;
-  velocityX[dotA] -= ratioX * impactSpeed;
-  velocityY[dotA] -= ratioY * impactSpeed;
-  velocityX[dotB] += ratioX * impactSpeed;
-  velocityY[dotB] += ratioY * impactSpeed;
-}
-function newDistanceSquared(delta2, a, b) {
-  newDotAx = posX[a] + velocityX[a] * delta2;
-  newDotAy = posY[a] + velocityY[a] * delta2;
-  newDotBx = posX[b] + velocityX[b] * delta2;
-  newDotBy = posY[b] + velocityY[b] * delta2;
-  return Math.abs(newDotAx - newDotBx) ** 2 + Math.abs(newDotAy - newDotBy) ** 2;
-}
-function renderFreeDot(i2) {
-  canvasCTX.beginPath();
-  canvasCTX.fillStyle = Color;
-  canvasCTX.strokeStyle = Color;
-  canvasCTX.lineWidth = Radius;
-  canvasCTX.moveTo(lastX[i2] - Radius, lastY[i2] - Radius);
-  canvasCTX.lineTo(posX[i2] - Radius, posY[i2] - Radius);
-  canvasCTX.stroke();
-  canvasCTX.closePath();
-  canvasCTX.fill();
-  lastX[i2] = posX[i2];
-  lastY[i2] = posY[i2];
-}
-function randomVelocity() {
-  return (Math.random() - 0.4) * CTX.MaxVelocity;
-}
 
 // src/dom.ts
-var $ = (id) => document.getElementById(id);
-var canvasCTX;
 function initCanvas() {
   const canvas = $("canvas-content");
   canvasCTX = canvas.getContext("2d");
@@ -338,19 +302,20 @@ function initDOM() {
     setAlpha(parseInt(trailsSlider.value));
   };
 }
+var $ = (id) => document.getElementById(id);
+var canvasCTX;
 
 // src/clockNumber.ts
-var dot = { x: 0, y: 0 };
 function createNumber(x, y) {
   const left = x;
   const top = y;
   let currentPixelMask;
   const dotLocations = new Array(MATRIX_HEIGHT);
-  for (let i2 = 0; i2 < MATRIX_HEIGHT; ++i2) {
-    dotLocations[i2] = new Array(MATRIX_WIDTH);
+  for (let i = 0;i < MATRIX_HEIGHT; ++i) {
+    dotLocations[i] = new Array(MATRIX_WIDTH);
   }
-  for (let y2 = 0; y2 < MATRIX_HEIGHT; ++y2) {
-    for (let x2 = 0; x2 < MATRIX_WIDTH; ++x2) {
+  for (let y2 = 0;y2 < MATRIX_HEIGHT; ++y2) {
+    for (let x2 = 0;x2 < MATRIX_WIDTH; ++x2) {
       const xx = left + x2 * DOT_WIDTH;
       const yy = top + y2 * DOT_HEIGHT;
       dotLocations[y2][x2] = {
@@ -363,8 +328,8 @@ function createNumber(x, y) {
     x: left,
     y: top,
     drawPixels: (newPixelMask) => {
-      for (y = 0; y < MATRIX_HEIGHT; ++y) {
-        for (x = 0; x < MATRIX_WIDTH; ++x) {
+      for (y = 0;y < MATRIX_HEIGHT; ++y) {
+        for (x = 0;x < MATRIX_WIDTH; ++x) {
           dot = dotLocations[y][x];
           if (currentPixelMask != null) {
             if (currentPixelMask[y][x] !== 0 && newPixelMask[y][x] === 0) {
@@ -380,8 +345,65 @@ function createNumber(x, y) {
     }
   };
 }
+var dot = { x: 0, y: 0 };
 
 // src/clockFace.ts
+function buildClockFace() {
+  initCanvas();
+  width = canvasCTX.canvas.clientWidth;
+  height = canvasCTX.canvas.clientHeight;
+  hours = [createNumber(0, 0), createNumber(0, 0)];
+  minutes = [createNumber(0, 0), createNumber(0, 0)];
+  seconds = [createNumber(0, 0), createNumber(0, 0)];
+  canvasCTX.fillStyle = "black";
+  canvasCTX.fillRect(0, 0, width, height);
+  canvasCTX.lineCap = "round";
+  createNumbers();
+  self.requestAnimationFrame(tick);
+}
+var tick = function(timestamp) {
+  canvasCTX.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
+  canvasCTX.fillRect(0, 0, width, height);
+  canvasCTX.fillStyle = "black";
+  renderDot(colon1X, currentY + 2 * DOT_HEIGHT);
+  renderDot(colon1X, currentY + 4 * DOT_HEIGHT);
+  renderDot(colon2X, currentY + 2 * DOT_HEIGHT);
+  renderDot(colon2X, currentY + 4 * DOT_HEIGHT);
+  updateTime(new Date);
+  tickDots(timestamp);
+  self.requestAnimationFrame(tick);
+};
+var updateTime = function(now) {
+  setDigits(pad2(now.getHours()), hours);
+  setDigits(pad2(now.getMinutes()), minutes);
+  setDigits(pad2(now.getSeconds()), seconds);
+};
+var setDigits = function(digits, numbers) {
+  numbers[0].drawPixels(PIXELS[parseInt(digits[0])]);
+  numbers[1].drawPixels(PIXELS[parseInt(digits[1])]);
+};
+var createNumbers = function() {
+  hSize = (DOT_WIDTH * MATRIX_WIDTH + NUMBER_SPACING) * 6 + (DOT_WIDTH + NUMBER_SPACING) * 2 - NUMBER_SPACING;
+  vSize = DOT_HEIGHT * MATRIX_HEIGHT;
+  currentY = (height - vSize) * 0.33;
+  currentX = (width - hSize) * 0.4;
+  buildNumber(hours);
+  colon1X = currentX + 8;
+  currentX += DOT_WIDTH + 2 * NUMBER_SPACING;
+  buildNumber(minutes);
+  colon2X = currentX + 8;
+  currentX += DOT_WIDTH + 2 * NUMBER_SPACING;
+  buildNumber(seconds);
+};
+var buildNumber = function(digits) {
+  for (i = 0;i < 2; ++i) {
+    digits[i] = createNumber(currentX, currentY);
+    currentX += DOT_WIDTH * MATRIX_WIDTH + NUMBER_SPACING;
+  }
+};
+var pad2 = function(num) {
+  return num < 10 ? "0" + num.toString() : num.toString();
+};
 var width;
 var height;
 var hSize = 0;
@@ -401,62 +423,6 @@ var minutes;
 var seconds;
 var colon1X = 0;
 var colon2X = 0;
-function buildClockFace() {
-  initCanvas();
-  width = canvasCTX.canvas.clientWidth;
-  height = canvasCTX.canvas.clientHeight;
-  hours = [createNumber(0, 0), createNumber(0, 0)];
-  minutes = [createNumber(0, 0), createNumber(0, 0)];
-  seconds = [createNumber(0, 0), createNumber(0, 0)];
-  canvasCTX.fillStyle = "black";
-  canvasCTX.fillRect(0, 0, width, height);
-  canvasCTX.lineCap = "round";
-  createNumbers();
-  self.requestAnimationFrame(tick);
-}
-function tick(timestamp) {
-  canvasCTX.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
-  canvasCTX.fillRect(0, 0, width, height);
-  canvasCTX.fillStyle = "black";
-  renderDot(colon1X, currentY + 2 * DOT_HEIGHT);
-  renderDot(colon1X, currentY + 4 * DOT_HEIGHT);
-  renderDot(colon2X, currentY + 2 * DOT_HEIGHT);
-  renderDot(colon2X, currentY + 4 * DOT_HEIGHT);
-  updateTime(/* @__PURE__ */ new Date());
-  tickDots(timestamp);
-  self.requestAnimationFrame(tick);
-}
-function updateTime(now) {
-  setDigits(pad2(now.getHours()), hours);
-  setDigits(pad2(now.getMinutes()), minutes);
-  setDigits(pad2(now.getSeconds()), seconds);
-}
-function setDigits(digits, numbers) {
-  numbers[0].drawPixels(PIXELS[parseInt(digits[0])]);
-  numbers[1].drawPixels(PIXELS[parseInt(digits[1])]);
-}
-function createNumbers() {
-  hSize = (DOT_WIDTH * MATRIX_WIDTH + NUMBER_SPACING) * 6 + (DOT_WIDTH + NUMBER_SPACING) * 2 - NUMBER_SPACING;
-  vSize = DOT_HEIGHT * MATRIX_HEIGHT;
-  currentY = (height - vSize) * 0.33;
-  currentX = (width - hSize) * 0.4;
-  buildNumber(hours);
-  colon1X = currentX + 8;
-  currentX += DOT_WIDTH + 2 * NUMBER_SPACING;
-  buildNumber(minutes);
-  colon2X = currentX + 8;
-  currentX += DOT_WIDTH + 2 * NUMBER_SPACING;
-  buildNumber(seconds);
-}
-function buildNumber(digits) {
-  for (i = 0; i < 2; ++i) {
-    digits[i] = createNumber(currentX, currentY);
-    currentX += DOT_WIDTH * MATRIX_WIDTH + NUMBER_SPACING;
-  }
-}
-function pad2(num) {
-  return num < 10 ? "0" + num.toString() : num.toString();
-}
 
 // src/main.ts
 initDOM();
